dc6c915ffcb50ec13a1d5e92f8707d557c124cae
dianamontero7 <dianamontero38@gmail.com>
2025-04-22 18:02:52 -0500

    initial commit

---
+++ Example/main.js
@@ -0,0 +1,16 @@
+// Operations: Object Access, Invocation
+console.log("What's your name?");
+
+// Operations: object access, invocation 
+process.stdin.on('data', function(data) {
+
+    // Operations: object access, invocation
+    const name = data.toString().trim();
+    
+    // Operations: object access, invocation
+    console.log(`Hello, ${name}!`);
+
+});
+
+
+
